c4135261f005f1731e9ce9f64a6170791a3bb093
Yashika Bihani <yashikabihani7@gmail.com>
2020-07-06 17:29:21 +0530

    Basic JS Structure

---
+++ app.js
@@ -0,0 +1,52 @@
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const productsDOM = document.querySelector('.products-center');
+
+let cart = [];
+
+//getting the products
+class Products{
+    async getProducts(){
+        try{
+            let result = await fetch('products.json')
+            let data = await result.json();
+            //destructing the products.json data
+            let products = data.items;
+            products = products.map(item => {
+                const {title,price} = item.fields;
+                const {id} = item.sys;
+                const image = item.fields.image.fields.file.url;
+                return {title,price,id,image}
+            })
+            return products;
+        }catch(error){
+            console.log(error);
+        }
+    }
+}
+
+//displaying products
+class UI {
+    displayProducts(products){
+        console.log(products)
+    }
+
+}
+
+//local storage
+class Storage{
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const ui = new UI();
+    const products = new Products();
+
+    //get all the products
+    products.getProducts().then(products => ui.displayProducts(products));
+})
